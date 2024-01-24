cc585cb097915de2f1b860681def130d421ca3c2
Vladyslav Lehkobyt <lighty1995@icloud.com>
2024-01-24 22:44:43 +0000

    Initial commit

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
+using System.Windows.Documents;
 
 
 namespace infograf
@@ -15,11 +16,32 @@ namespace infograf
 
         private int currentCase = 0;
         private bool isChartInitialized = false;
+        int barCount = 0;
 
         public MainWindow()
         {
             InitializeComponent();
             AnimateButtons();
+            CreateCaseIndicator(); // Створення індикатора кейсу
+        }
+
+        private TextBlock? caseIndicator; // Індикатор кейсу
+
+        private void CreateCaseIndicator()
+        {
+            caseIndicator = new TextBlock
+            {
+                FontWeight = FontWeights.Bold,
+                FontSize = 16,
+                HorizontalAlignment = HorizontalAlignment.Right,
+                VerticalAlignment = VerticalAlignment.Top,
+                Margin = new Thickness(0, 10, 0, 0)
+            };
+
+            Canvas.SetTop(caseIndicator, 10);
+            Canvas.SetRight(caseIndicator, 10);
+
+            MyCanvas.Children.Add(caseIndicator);
         }
 
 
@@ -52,52 +74,72 @@ namespace infograf
 
         private void UpdateBars()
         {
-            MyCanvas.Children.Clear(); // Очищення поточних барів
+            MyCanvas.Children.Clear();
+            CreateCaseIndicator();
+
+            barCount = 0;
 
             // Оновлення висот барів залежно від поточного кейсу
-            switch (currentCase)
+            if (caseIndicator != null)
             {
-                case 0:
-                    break;
-                case 1:
-                    // Висоти для кейсу 1
-                    AddBar(50, 70, 10);
-                    AddBar(170, 70, 30);
-                    AddBar(290, 70, 100);
-                    AddBar(410, 70, 100);
-                    break;
-                case 2:
-                    // Висоти для кейсу 2
-                    AddBar(50, 70, 20);
-                    AddBar(170, 70, 40);
-                    AddBar(290, 70, 50);
-                    AddBar(410, 70, 30);
-                    break;
-                case 3:
-                    // Висоти для кейсу 3
-                    AddBar(50, 70, 50);
-                    AddBar(170, 70, 20);
-                    AddBar(290, 70, 36);
-                    AddBar(410, 70, 34);
-                    break;
-                case 4:
-                    // Висоти для кейсу 4
-                    AddBar(50, 70, 45);
-                    AddBar(170, 70, 23);
-                    AddBar(290, 70, 98);
-                    AddBar(410, 70, 100);
-                    break;
-                case 5:
-                    // Висоти для кейсу 5
-                    AddBar(50, 70, 27);
-                    AddBar(170, 70, 45);
-                    AddBar(290, 70, 23);
-                    AddBar(410, 70, 78);
-                    break;
-                default:
-                    break;
+                switch (currentCase)
+                {
+                    case 0:
+                        break;
+                    case 1:
+                        // Висоти для кейсу 1
+                        caseIndicator.Text = "Оплата праці";
+                        AddBar(50, 70, 76);
+                        AddBar(170, 70, 81);
+                        AddBar(290, 70, 89);
+                        AddBar(410, 70, 82);
+                        UpdateInfoText("Оплата праці – це витрати державного бюджету на заробітну плату працівників державного сектору. " +
+                            "Включає заробітну плату, податки та соціальні внески, які платить держава.", 
+                            "https://lb.ua/blog/tetiana_bohdan/505515_byudzhetni_vidatki_2021_roku_zleti_i.html");
+                        break;
+                    case 2:
+                        // Висоти для кейсу 2
+                        caseIndicator.Text = "Субсидії та поточні трансферти підприємствам";
+                        AddBar(50, 70, 14);
+                        AddBar(170, 70, 20);
+                        AddBar(290, 70, 32);
+                        AddBar(410, 70, 22);
+                        UpdateInfoText("Субсидії та поточні трансферти підприємствам – це державні виплати, які не пов'язані з придбанням " +
+                            "товарів чи послуг. Це включає дотації підприємствам для підтримки їх конкурентоспроможності, допомогу в складних " +
+                            "фінансових умовах, стимулювання економічного розвитку.", 
+                            "https://lb.ua/blog/tetiana_bohdan/505515_byudzhetni_vidatki_2021_roku_zleti_i.html");
+                        break;
+                    case 3:
+                        // Висоти для кейсу 3
+                        caseIndicator.Text = "Соціальне забезпечення";
+                        AddBar(50, 70, 84);
+                        AddBar(170, 70, 79);
+                        AddBar(290, 70, 79);
+                        AddBar(410, 70, 68);
+                        UpdateInfoText("Соціальне забезпечення – це державні витрати на підтримку громадян у випадках, які вимагають соціальної " +
+                            "допомоги: пенсії, допомога по безробіттю, соціальні виплати малозабезпеченим, інвалідам та іншим соціально " +
+                            "вразливим категоріям населення.",
+                            "https://lb.ua/blog/tetiana_bohdan/505515_byudzhetni_vidatki_2021_roku_zleti_i.html");
+                        break;
+                    case 4:
+                        // Висоти для кейсу 4
+                        caseIndicator.Text = "Капітальні видатки";
+                        AddBar(50, 70, 41);
+                        AddBar(170, 70, 39);
+                        AddBar(290, 70, 40);
+                        AddBar(410, 70, 40);
+                        UpdateInfoText("Капітальні видатки – це частина державних видатків, які спрямовуються на придбання або підтримку " +
+                            "довгострокових активів, таких як будівництво інфраструктури, закупівля обладнання, реконструкція та модернізація " +
+                            "існуючих об'єктів.",
+                            "https://lb.ua/blog/tetiana_bohdan/505515_byudzhetni_vidatki_2021_roku_zleti_i.html");
+                        break;
+                    default:
+                        caseIndicator.Text = "";
+                        break;
+                }
             }
-        }
+        }   
+
 
         private void AddBar(double x, double width, double originalMaxHeight)
         {
@@ -124,10 +166,10 @@ namespace infograf
                 Foreground = new SolidColorBrush(Colors.Black),
                 FontWeight = FontWeights.Bold,
                 HorizontalAlignment = HorizontalAlignment.Center,
-                VerticalAlignment = VerticalAlignment.Top
+                VerticalAlignment = VerticalAlignment.Bottom
             };
 
-            Canvas.SetLeft(percentageText, x + (width / 2) - (percentageText.ActualWidth / 2));
+            Canvas.SetLeft(percentageText, x + width / 2);
             Canvas.SetTop(percentageText, startY - 20); // Розташування тексту над баром
 
             MyCanvas.Children.Add(percentageText);
@@ -159,6 +201,24 @@ namespace infograf
             {
                 solidColorBrush.BeginAnimation(SolidColorBrush.ColorProperty, colorAnimation);
             }
+
+            string[] years = { "2018", "2019", "2020", "2021" };
+            if (barCount < years.Length)
+            {
+                TextBlock yearLabel = new TextBlock
+                {
+                    Text = years[barCount],
+                    Foreground = new SolidColorBrush(Colors.Black),
+                    FontWeight = FontWeights.Bold,
+                    HorizontalAlignment = HorizontalAlignment.Center
+                };
+
+                Canvas.SetLeft(yearLabel, x + 20);
+                Canvas.SetTop(yearLabel, 380); // Можна налаштувати відповідно до вашого дизайну
+                MyCanvas.Children.Add(yearLabel);
+            }
+
+            barCount++;
         }
 
 
@@ -186,7 +246,7 @@ namespace infograf
         {
             if (currentCase == 0 || currentCase == 1)
             {
-                currentCase = 5;
+                currentCase = 4;
             }
             else
             {
@@ -197,15 +257,47 @@ namespace infograf
 
         private void ForwardButton_Click(object sender, RoutedEventArgs e)
         {
-            if (currentCase == 0 || currentCase == 5)
+            if (currentCase == 0 || currentCase == 4)
             {
                 currentCase = 1;
             }
-            else if (currentCase < 5) // Тепер максимальне значення - 5
+            else if (currentCase < 4) // Тепер максимальне значення - 4
             {
                 currentCase++;
             }
             CreateBarChart();
         }
+
+        public void UpdateInfoText(string description, string source)
+        {
+            // Очищаємо попередній контент
+            InfoTextBlock.Inlines.Clear();
+
+            // Додаємо опис
+            InfoTextBlock.Inlines.Add(new Run($"Опис показника: {description}\n"));
+
+            // Створюємо гіперпосилання
+            Hyperlink sourceLink = new Hyperlink(new Run($"Джерело даних: {source}"))
+            {
+                NavigateUri = new Uri(source) // Припускаючи, що 'source' це валідний URL
+            };
+            // Використовуємо новий обробник подій
+            sourceLink.RequestNavigate += Hyperlink_RequestNavigate;
+
+            // Додаємо гіперпосилання до TextBlock
+            InfoTextBlock.Inlines.Add(sourceLink);
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+        {
+            var psi = new System.Diagnostics.ProcessStartInfo
+            {
+                FileName = e.Uri.AbsoluteUri,
+                UseShellExecute = true
+            };
+            System.Diagnostics.Process.Start(psi);
+            e.Handled = true;
+        }
+
     }
 }
